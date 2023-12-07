76c38a595863f40fdf501cfa40bc696330225807
devZahid <officialzahidhasan19932021@gmail.com>
2023-12-07 15:10:42 +0600

    properly config tailwindcss file and created theme.css and globals.css to use custom class name using tailwindcss classes

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="container mx-auto">
+        {children}
+        </body>
     </html>
   )
 }
