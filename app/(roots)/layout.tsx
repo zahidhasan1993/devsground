f54135804665fd8a6039f205c6b4473d23e7b45b
devZahid <officialzahidhasan19932021@gmail.com>
2023-12-21 13:47:41 +0600

    adding rightsidebar

--- app/(roots)/layout.tsx
+++ app/(roots)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSideBar from "@/components/shared/LeftSideBar";
 import Navbar from "@/components/shared/Navbar";
+import RightSideBar from "@/components/shared/RightSideBar";
 import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -11,7 +12,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        <div className="">rightSideBar</div>
+        <RightSideBar />
       </div>
     </main>
   );
---
+++ components/shared/RightSideBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RightSideBar = () => {
+  return (
+    <section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 custom-scrollbar shadow-light-300 dark:shadow-none max-xl:hidden">
+      <h1>Hellow</h1>
+    </section>
+  );
+};
+
+export default RightSideBar;
