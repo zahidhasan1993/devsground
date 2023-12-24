d0238b17c2fc5dc66d2d898b49304ec92c261a0f
devZahid <officialzahidhasan19932021@gmail.com>
2023-12-24 12:45:33 +0600

    styling home component

--- app/(roots)/(home)/page.tsx
+++ app/(roots)/(home)/page.tsx
@@ -1,11 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="flex justify-between mt-5">
-      <h1 className="h1-bold">This is Home</h1>
-      <div className=""></div>
-    </div>
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">Ask-Questions</h1>
+        <Link href="/ask-questions" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      {/* div for search bar and content */}
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center"></div>
+    </>
   );
 };
 
